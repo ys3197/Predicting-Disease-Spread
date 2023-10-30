c13d688928938e53e79b2433348c8d083c896a77
ys3197 <43221315+ys3197@users.noreply.github.com>
2023-10-30 02:22:48 -0400

    Add files via upload

--- utils/feature_engineer.py
+++ utils/feature_engineer.py
@@ -6,6 +6,8 @@ from __future__ import division
 
 import pandas as pd
 import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import FunctionTransformer
 
 from warnings import filterwarnings
 filterwarnings('ignore')
@@ -22,12 +24,12 @@ def create_weekly_features(dataset):
     data_cur_year = dataset[dataset.year == year]
     ## no historical data for first year
     if year == dataset.year.unique()[0]:
-      data_cur_year['weekly_median_cases'] = None
+      data_cur_year['weekly_median_precip'] = None
       # data_cur_year['monthly_ave_cases'] = None
 
     ## only focusing on historical data to avoid data leakage
     else:
-      week_median = dataset[dataset.year < year].groupby(['weekofyear'])['total_cases'].median().to_frame().rename(columns={'total_cases': 'weekly_median_cases'}).reset_index()
+      week_median = dataset[dataset.year < year].groupby(['weekofyear'])['precipitation_amt_mm'].median().to_frame().rename(columns={'precipitation_amt_mm': 'weekly_median_precip'}).reset_index()
       # month_ave = dataset[dataset.year < year].groupby(['month'])['total_cases'].mean().to_frame().rename(columns={'total_cases': 'monthly_ave_cases'}).reset_index()
       data_cur_year = data_cur_year.merge(week_median, how='left', on='weekofyear')
       # data_cur_year = data_cur_year.merge(month_ave, how='left', on='month')
@@ -35,6 +37,33 @@ def create_weekly_features(dataset):
     updated_df = pd.concat([updated_df, data_cur_year], ignore_index=True)
 
   return updated_df
+  
+
+def feature_transformation(dataset, right_skew_col, left_skew_col):
+  """
+  This function is to deal with skewed features
+  """
+  ## log transformation
+  logTr = ColumnTransformer(transformers=[("lg", FunctionTransformer(np.log1p), right_skew_col)])
+  ## Square transformation
+  sqrTr = ColumnTransformer(transformers=[("lg", FunctionTransformer(np.square), left_skew_col)])
+  ## fit and transform
+  dataset[right_skew_col] = logTr.fit_transform(dataset)
+  dataset[left_skew_col] = sqrTr.fit_transform(dataset)
+  
+  return dataset
+  
+
+def time_related_features(dataset):
+  """
+  This function is to generate time-related features
+  """
+  ## rolling mean of two weeks
+  dataset['station_precip_mm_rolling_month_mean'] = dataset['station_precip_mm'].rolling(2, min_periods=1).mean()
+  ## difference of precipitation
+  dataset['station_precip_mm_diff'] = dataset['station_precip_mm'].diff().fillna(0)
+  
+  return dataset
 
 
 def remove_features(dataset):
@@ -43,8 +72,7 @@ def remove_features(dataset):
   """
   ## drop tempearture related features
   dataset.drop(columns = ['reanalysis_avg_temp_k',
-                          # 'reanalysis_tdtr_k',
-                          'reanalysis_sat_precip_amt_mm', 'reanalysis_specific_humidity_g_per_kg'], inplace=True)
+                          'reanalysis_sat_precip_amt_mm'], inplace=True)
   ## drop columns with > 20 % missing values
   dataset.drop(columns = ['ndvi_ne'], inplace=True)
   ## drop some time-related features
@@ -57,7 +85,8 @@ def feature_crossing(dataset):
   """
   This function is to create some feature interactions
   """
-  dataset['reanalysis_hot_humid_index'] = dataset['reanalysis_relative_humidity_percent']*dataset['reanalysis_tdtr_k']
+  dataset['reanalysis_temp_humid_index'] = dataset['reanalysis_relative_humidity_percent']*dataset['reanalysis_tdtr_k']
+  dataset['ndvi_max_precip'] = dataset.apply(lambda row: max(row['ndvi_se'], row['ndvi_sw'], row['ndvi_nw']) * row['precipitation_amt_mm'], axis=1)
 
   return dataset
 
@@ -75,12 +104,14 @@ def other_preprocess(dataset, multiple_models=False):
   return dataset
 
 
-def feature_engineer_pipeline(dataset, multiple_models=False):
+def feature_engineer_pipeline(dataset, right_skew_col, left_skew_col, multiple_models=False):
   """
   This function is to aggregrate all feature engineering
   """
+  dataset = feature_transformation(dataset, right_skew_col, left_skew_col)
   dataset = remove_features(dataset)
   dataset = feature_crossing(dataset)
+  dataset = time_related_features(dataset)
   dataset = other_preprocess(dataset, multiple_models)
 
   return dataset
